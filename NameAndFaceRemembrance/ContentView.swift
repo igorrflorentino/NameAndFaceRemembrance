e58f56dfdc45d92b199319f19e6cb7c4f66994d3
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-07-27 19:50:28 -0300

    Initial Commit

---
+++ NameAndFaceRemembrance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NameAndFaceRemembrance
+//
+//  Created by Igor Florentino on 27/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NameAndFaceRemembrance/NameAndFaceRemembranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameAndFaceRemembranceApp.swift
+//  NameAndFaceRemembrance
+//
+//  Created by Igor Florentino on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NameAndFaceRemembranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
